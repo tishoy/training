5cd9e2eede7f3febc3054c5dd273e489c671add3
Tishoy Han <hantishoy@sina.com>
2017-10-13 09:48:51 +0800

    修改测试时 使用路由

--- test/routes.js
+++ test/routes.js
@@ -6,35 +6,35 @@ var assert = require('assert');
 let expect = chai.expect;
 
 let routers = "http://47.93.26.208:8001/index.php?m=train&c=route&a=client_route";
-let addr = ""
+let addr = "http://47.93.26.208:8001/index.php?m=train";
 var routes = {
     "login": "http://47.93.26.208:8001/index.php?m=train&c=users&a=login",
     "register": "http://47.93.26.208:8001/index.php?m=train&c=users&a=regist",
     "available": "http://47.93.26.208:8001/index.php?m=train&c=users&a=available",
-    "logout": "",
-    "reset": addr + "users/reset",
-    "info": addr + "users/info",
+    "logout": addr + "&c=users&a=logout",
+    "reset": addr + "&c=users&a=reset",
+    "info": addr + "&c=users&a=info",
 
-    "insert": addr + "students/insert",
-    "remove": addr + "students/remove",
-    "base": addr + "students/base",
-    "self": addr + "students/self",
-    "addexp": addr + "students/addexp",
-    "delexp": addr + "students/delexp",
+    "insert": addr + "&c=students&a=insert",
+    "remove": addr + "&c=students&a=remove",
+    "base": addr + "&c=students&a=base",
+    "self": addr + "&c=students&a=self",
+    "addexp": addr + "&c=students&a=addexp",
+    "delexp": addr + "&c=students&a=delexp",
 
-    "examing": addr + "exams/examing",
-    "pass": addr + "exams/pass",
-    "retry": addr + "exams/retry",
-    "score": addr + "exams/score",
-    "over": addr + "exams/over",
+    "examing": addr + "&c=exams&a=examing",
+    "pass": addr + "&c=exams&a=pass",
+    "retry": addr + "&c=exams&a=retry",
+    "score": addr + "&c=exams&a=score",
+    "over": addr + "&c=exams&a=over",
 
-    "enroll": addr + "enrolled/enroll",
-    "agree": addr + "enrolled/agree",
-    "refuse": addr + "enrolled/refuse",
+    "enroll": addr + "&c=enrolled&a=enroll",
+    "agree": addr + "&c=enrolled&a=agree",
+    "refuse": addr + "&c=enrolled&a=refuse",
 
-    "new": addr + "clazz/new",
-    "entrance": addr + "clazz/entrance",
-    "exit": addr + "clazz/exit",
+    "new": addr + "&c=clazz&a=new",
+    "entrance": addr + "&c=clazz&a=entrance",
+    "exit": addr + "&c=clazz&a=exit",
 
     "query": "http://47.93.26.208:8001/index.php?m=train&c=query&a=info",
 }
@@ -80,35 +80,60 @@ describe('服务器API测试', function () {
         });
     });
 
-    // it('请求注册', function () {
-    //     return fetch(routes.register, Object.assign(header,
-    //         { body: JSON.stringify({ account: "tishoy", password: "hantishoy123", type: 1 }) }
-    //     )).then(function (res) {
-    //         return res.json();
-    //     }).then(function (json) {
-    //         expect(json).to.be.an('object');
-    //     });
-    // });
+    it('请求注册', function () {
+        return fetch(routes.register, Object.assign(header,
+            { body: JSON.stringify({ account: "tishoy", password: "hantishoy123", type: 1 }) }
+        )).then(function (res) {
+            return res.json();
+        }).then(function (json) {
+            expect(json).to.be.an('object');
+        });
+    });
 
-    // it('用户名可用', function () {
-    //     return fetch(routes.available, Object.assign(header,
-    //         { body: JSON.stringify({ account: "tishoy", password: "hantishoy123", type: 1 }) }
-    //     )).then(function (res) {
-    //         return res.json();
-    //     }).then(function (json) {
-    //         expect(json).to.be.an('object');
-    //     });
-    // });
+    it('用户名可用', function () {
+        return fetch(routes.available, Object.assign(header,
+            { body: JSON.stringify({ account: "tishoy", password: "hantishoy123", type: 1 }) }
+        )).then(function (res) {
+            return res.json();
+        }).then(function (json) {
+            expect(json).to.be.an('object');
+        });
+    });
 
-    // it('用户登出', function () {
-    //     return fetch(routes.logout, Object.assign(header,
-    //         { body: JSON.stringify({ session: "tishoy" }) }
-    //     )).then(function (res) {
-    //         return res.json();
-    //     }).then(function (json) {
-    //         expect(json).to.be.an('object');
-    //     });
-    // });
+    it('用户登出', function () {
+        return fetch(routes.logout, Object.assign(header,
+            { body: JSON.stringify({ session: "tishoy" }) }
+        )).then(function (res) {
+            return res.json();
+        }).then(function (json) {
+            expect(json).to.be.an('object');
+        });
+    });
+
+    it('用户设置', function () {
+        return fetch(routes.reset, Object.assign(header,
+            { body: JSON.stringify({ session: "tishoy" }) }
+        )).then(function (res) {
+            return res.json();
+        }).then(function (json) {
+            expect(json).to.be.an('object');
+        });
+    });
+
+    it('用户信息', function () {
+        return fetch(routes.info, Object.assign(header,
+            { body: JSON.stringify({ session: "tishoy" }) }
+        )).then(function (res) {
+            return res.json();
+        }).then(function (json) {
+            expect(json).to.be.an('object');
+        });
+    });
+
+
+    it('插入学生', function() {
+        return 
+    })
 
     it('请求数据', function () {
         return fetch(routes.query, Object.assign(header, {
