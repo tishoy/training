ef99538e4d61595b8e5469d9d009e09c0b2ee405
ChuYLion <cyl_txt@sina.com>
2017-11-02 22:21:36 +0800

    aa

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
-var config = {"routers":"http://47.93.26.208:8001/index.php?m=train&c=route&a=client_route","notification":"test.php","language":"Chin","version":"9101c2a"};
+var config = {"routers":"http://47.93.26.208:8001/index.php?m=train&c=route&a=client_route","notification":"test.php","language":"Chin","version":"2bcc4ef"};
 module.exports = config;
-// var config = {"routers":"http://192.168.4.183/index.php?m=train&c=route&a=client_route","notification":"test.php","language":"Chin","version":"9101c2a"};
-// module.exports = config;
