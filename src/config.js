c0a5438c8fccae8ce8c3a67aadc6b0e5ae41d15d
ChuYLion <cyl_txt@sina.com>
2017-11-06 13:46:36 +0800

    change v code

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-var config = {"routers":"http://47.93.26.208:8001/index.php?m=train&c=route&a=client_route","notification":"test.php","language":"Chin","version":"573d283"};
+var config = {"routers":"http://47.93.26.208:8001/index.php?m=train&c=route&a=client_route","notification":"test.php","language":"Chin","version":"bc926aa"};
 module.exports = config;
