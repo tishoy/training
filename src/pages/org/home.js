5f4ecedd79af8fda77ab003be6064152ec2c8ce2
liuxuesong <liuxuesong930721@163.com>
2017-11-14 16:02:39 +0800

    add_z_max_

--- src/pages/org/home.js
+++ src/pages/org/home.js
@@ -27,6 +27,8 @@ class Home extends Component {
         name: "",
         arranged_nums: 0,
         all_students_nums: 0,
+        m_max:0,
+        h_max:0,
         registered_nums: 0,
         all_registered_nums: 0,
         clazzes: [],
@@ -57,6 +59,8 @@ class Home extends Component {
                 clazzes: window.CacheData.clazzes,
                 arranged_nums: message.data.info.sum.all_arrange_count,
                 all_students_nums: message.data.info.sum.all_count,
+                m_max:message.data.info.sum.m_max,
+                h_max:message.data.info.sum.h_max,
                 //已临时登记人数
                 registered_nums:  message.data.info.sum.registered_nums,
                 all_registered_nums: message.data.info.sum.all_registered_nums
@@ -117,14 +121,22 @@ class Home extends Component {
                         <div className="nyx-areacount-title">各省市报名情况</div>
                         {this.state.areas.map(area => {
                             var all_num = this.state.all_students_nums;
+                            var m_max =this.state.m_max;
+                            var h_max = this.state.h_max;
+                            var z_max;
+                            if(parseInt(m_max)>parseInt(h_max)){
+                                z_max=m_max;
+                            }else{
+                                z_max=h_max;
+                            }
                             var m_all_count = area.m_count ? area.m_count : 0;
                             var m_arrange_count = area.m_arrange_count ? area.m_arrange_count : 0;
                             var h_all_count = area.h_count ? area.h_count : 0;
                             var h_arrange_count = area.h_arrange_count ? area.h_arrange_count : 0;
-                            var m_pre_all = 100 * m_all_count / all_num;
-                            var m_pre_arr = 100 * m_arrange_count / all_num;
-                            var h_pre_all = 100 * h_all_count / all_num;
-                            var h_pre_arr = 100 * h_arrange_count / all_num;
+                            var m_pre_all = 100 * m_all_count / z_max;
+                            var m_pre_arr = 100 * m_arrange_count / z_max;
+                            var h_pre_all = 100 * h_all_count / z_max;
+                            var h_pre_arr = 100 * h_arrange_count / z_max;
 
                             return (
                                 <div key={area.area_id} className="nyx-areacount-list-item">
